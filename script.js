1f593c79bb9d9218b6cc203dbb6b2cc38665cc86
Chris Stewart <c.stewart5545@gmail.com>
2021-08-07 11:45:44 -0500

    app basic functional

--- script.js
+++ script.js
@@ -95,10 +95,9 @@ function handleModalAdd(){
     const author = document.getElementById('author').value;
     const pages = document.getElementById('pages').value;
     const hasRead = document.getElementById('hasRead').checked;
-    console.log(title.valueMissing)
+    if(validateForm())return;
     addBooktoLibrary(title.value,author,pages,hasRead);
-    toggleModalAppearance();
-       
+    toggleModalAppearance(); 
 }
 
 function hasError (field) {
@@ -108,89 +107,90 @@ function hasError (field) {
     // Get validity
     var validity = field.validity;
 
-        // If valid, return null
-if (validity.valid) return;
+    // If valid, return null
 
-// If field is required and empty
-if (validity.valueMissing) return 'Please fill out this field.';
+    if (validity.valid) return;
 
-// If not the right type
-if (validity.typeMismatch) return 'Please use the correct input type.';
+    if(field.type ==="checkbox") return;
 
-// If too short
-if (validity.tooShort) return 'Please lengthen this text.';
+    if(field.type ==="number") return 'Please select a value that is no less than ' + field.getAttribute('min') + '.';
 
-// If too long
-if (validity.tooLong) return 'Please shorten this text.';
+    if(field.id === "title") return "Please enter a book title";
 
-// If number input isn't a number
-if (validity.badInput) return 'Please enter a number.';
+    if(field.id === "author") return "Please enter an author's name"
 
-// If a number value doesn't match the step interval
-if (validity.stepMismatch) return 'Please select a valid value.';
-
-// If a number field is over the max
-if (validity.rangeOverflow) return 'Please select a smaller value.';
-
-// If a number field is below the min
-if (validity.rangeUnderflow) return 'Please select a larger value.';
-
-// If pattern doesn't match
-if (validity.patternMismatch) return 'Please match the requested format.';
-
-// If a number field is below the min
-if (validity.rangeUnderflow) return 'Please select a value that is no less than ' + field.getAttribute('min') + '.';
-
-// If all else fails, return a generic catchall error
-return 'The value you entered for this field is invalid.';
+    // If all else fails, return a generic catchall error
+    return 'The value you entered for this field is invalid.';
 
 }
 
 function showError (field,error){
-field.classList.add('error');
+    field.classList.add('error');
 
-let id = field.id;
-if(!id) return;
+    let id = field.id;
+    if(!id) return;
 
-// Check if error message field already exists
-// If not, create one
-var message = field.form.querySelector('.error-message#error-for-' + id );
-if (!message) {
-    message = document.createElement('div');
-    message.className = 'error-message';
-    message.id = 'error-for-' + id;
-    field.parentNode.insertBefore( message, field.nextSibling );
-}
+    // Check if error message field already exists
+    // If not, create one
+    var message = field.form.querySelector('.error-message#error-for-' + id );
+    if (!message) {
+        message = document.createElement('div');
+        message.className = 'error-message';
+        message.id = 'error-for-' + id;
+        field.parentNode.insertBefore( message, field.nextSibling );
+    }
 
-// Update error message
-message.innerHTML = error;
+    // Update error message
+    message.innerHTML = error;
 
-// Show error message
-message.style.display = 'block';
-message.style.visibility = 'visible';
+    // Show error message
+    message.style.display = 'block';
+    message.style.visibility = 'visible';
 
 };
 
 function removeError (field) {
-field.classList.remove('error');
+    field.classList.remove('error');
 
-// Remove ARIA role from the field
-field.removeAttribute('aria-describedby');
+    // Remove ARIA role from the field
+    field.removeAttribute('aria-describedby');
 
-// Get field id or name
-var id = field.id || field.name;
-if (!id) return;
+    // Get field id or name
+    var id = field.id || field.name;
+    if (!id) return;
 
-// Check if an error message is in the DOM
-var message = field.form.querySelector('.error-message#error-for-' + id + '');
-if (!message) return;
+    // Check if an error message is in the DOM
+    var message = field.form.querySelector('.error-message#error-for-' + id + '');
+    if (!message) return;
 
-// If so, hide it
-message.innerHTML = '';
-message.style.display = 'none';
-message.style.visibility = 'hidden';
+    // If so, hide it
+    message.innerHTML = '';
+    message.style.display = 'none';
+    message.style.visibility = 'hidden';
+};
+
+function validateForm() {
+    // Get all of the form elements
+    let fields = modalForm.elements;
+
+    // Validate each field
+    // Store the first field with an error to a variable so we can bring it into focus later
+    let error, hasErrors;
+    for (let i = 0; i < fields.length; i++) {
+        error = hasError(fields[i]);
+        if (error) {
+            showError(fields[i], error);
+            if (!hasErrors) {
+                hasErrors = fields[i];
+            }
+        }
+    }
+    // If there are errrors, don't submit form and focus on first element with error
+    if (hasErrors) {
+        hasErrors.focus();
+        return true
+    }
 }
-
 
 
 
@@ -205,14 +205,15 @@ addBooktoLibrary("LOTR: Return of the King","J.R.R Tolkien",308,true)
 
 // MODAL EVENT LISTENERS//
 
+modalAdd.addEventListener('click',handleModalAdd,false)
 newBtn.addEventListener('click',toggleModalAppearance)
 modalx.addEventListener('click',toggleModalAppearance)
-modalAdd.addEventListener('click',handleModalAdd)
+//modalAdd.addEventListener('click',handleModalAdd)
 window.addEventListener('click',(e)=>{
     if(e.target == bookModal) toggleModalAppearance();
 })
 
-document.addEventListener('blur',
+bookModal.addEventListener('blur',
 function (e) {
     //Validtate Error
     let error = hasError(e.target);
@@ -227,34 +228,5 @@ function (e) {
 
 }, true);
 
-document.addEventListener('submit', function (event) {
 
-    // Only run on forms flagged for validation
-    if (!event.target.classList.contains('validate')) return;
 
-    // Get all of the form elements
-    var fields = event.target.elements;
-
-    // Validate each field
-    // Store the first field with an error to a variable so we can bring it into focus later
-    var error, hasErrors;
-    for (var i = 0; i < fields.length; i++) {
-        error = hasError(fields[i]);
-        if (error) {
-            showError(fields[i], error);
-            if (!hasErrors) {
-                hasErrors = fields[i];
-            }
-        }
-    }
-
-    // If there are errrors, don't submit form and focus on first element with error
-    if (hasErrors) {
-        event.preventDefault();
-        hasErrors.focus();
-    }
-
-    // Otherwise, let the form submit normally
-    // You could also bolt in an Ajax form submit process here
-
-}, false);
